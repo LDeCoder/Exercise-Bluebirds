120be2ffbffc9baa64843eb4966faf3dea98db1f
LoriDeCoder <ldavis1005@gmail.com>
2018-06-30 14:27:22 -0500

    Added code - it works!

---
+++ script.js
@@ -0,0 +1,10 @@
+var awesomeBirds= ["Bluebird","Roadrunner","Desert Dove","Pigeon"];
+var i ="awesomeBirds[0]";
+for(i = 0; i < awesomeBirds.length; i++) {
+ if (awesomeBirds[i] === "Bluebird") {
+   alert("Bluebirds are so beautiful");
+ }
+ else {
+   alert ("Not as beautiful as Bluebirds");
+ }
+}
